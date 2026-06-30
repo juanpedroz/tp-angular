64f9ea23493fb204352a46e5ddf6d073ec01dd18
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-06-30 10:17:19 -0300

    1er componente

---
+++ repuesterio/src/app/lista-repuestos/lista-repuestos.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-repuestos',
+  imports: [],
+  templateUrl: './lista-repuestos.html',
+  styleUrl: './lista-repuestos.scss',
+})
+export class ListaRepuestos {}
