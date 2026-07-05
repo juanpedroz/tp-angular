2ec3373648890d3d3c18580dae1235741926bf6a
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-07-05 09:28:16 -0300

    abstraccion btn-input-btn

--- repuesterio/src/app/input-integer/input-integer.ts
+++ repuesterio/src/app/input-integer/input-integer.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Repuesto } from '../lista-repuestos/Repuesto';
 import { FormsModule } from '@angular/forms';
 
@@ -9,17 +9,35 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './input-integer.scss',
 })
 export class InputInteger {
-  @Input() repuesto!: Repuesto;
+  @Input() cantidad!: number;
+  @Input() max!: number;
 
-  sumarCantidad(repuesto: Repuesto) {
-    if (repuesto.cantidad < repuesto.stock) {
-      repuesto.cantidad++;
+  @Output() cantidadChange = new EventEmitter<number>();
+
+  sumarCantidad() {
+    if (this.cantidad < this.max) {
+      this.cantidad++;
+      this.cantidadChange.emit(this.cantidad);
     }
   }
 
-  restarCantidad(repuesto: Repuesto) {
-    if (repuesto.cantidad > 0) {
-      repuesto.cantidad--;
+  restarCantidad() {
+    if (this.cantidad > 0) {
+      this.cantidad--;
+      this.cantidadChange.emit(this.cantidad);
     }
   }
+  // @Input() repuesto!: Repuesto;
+
+  // sumarCantidad(repuesto: Repuesto) {
+  //   if (repuesto.cantidad < repuesto.stock) {
+  //     repuesto.cantidad++;
+  //   }
+  // }
+
+  // restarCantidad(repuesto: Repuesto) {
+  //   if (repuesto.cantidad > 0) {
+  //     repuesto.cantidad--;
+  //   }
+  // }
 }
