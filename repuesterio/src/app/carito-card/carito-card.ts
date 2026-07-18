8b60b3109bdff72f47f2c74a1df631793eef42c6
Juan Pedro Zugbi <jzugbi@gmail.com>
2026-07-18 09:09:36 -0300

    cambios side-bar y otros

--- repuesterio/src/app/carito-card/carito-card.ts
+++ repuesterio/src/app/carito-card/carito-card.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NgFor, NgIf } from '@angular/common';
 import { CarritoRepuestos } from '../carrito-repuesto';
 import { Repuesto } from '../lista-repuestos/Repuesto';
-import {AsyncPipe} from "@angular/common";
+import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs/internal/Observable';
 
 @Component({
@@ -13,9 +13,7 @@ import { Observable } from 'rxjs/internal/Observable';
   styleUrl: './carito-card.scss',
 })
 export class CaritoCard {
-
   listaCarrito$: Observable<Repuesto[]>;
-
 
   constructor(private carritoRepuestos: CarritoRepuestos) {
     this.listaCarrito$ = carritoRepuestos.listaCarrito.asObservable();
--- repuesterio/src/app/side-bar-repuesterio/side-bar-repuesterio.ts
+++ repuesterio/src/app/side-bar-repuesterio/side-bar-repuesterio.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CaritoCard } from '../carito-card/carito-card';
 import { ResumenCard } from '../resumen-card/resumen-card';
-import { PromoCard } from '../promo-card/promo-card';
 
 @Component({
   selector: 'app-side-bar-repuesterio',
   standalone: true,
-  imports: [CaritoCard,ResumenCard,PromoCard],
+  imports: [CaritoCard,ResumenCard],
   templateUrl: './side-bar-repuesterio.html',
   styleUrl: './side-bar-repuesterio.scss',
 })
